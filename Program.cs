5c06ac3aa8cbb188b99ae9f18def96e16951c763
Steve Beale <beale.stephen@gmail.com>
2024-08-12 15:36:55 +0100

    Added some sales data

--- Program.cs
+++ Program.cs
@@ -14,9 +14,18 @@ decimal val2 = 1234.5678m;
  * G = general
  * etc
  */
-Console.WriteLine($"{val1:D}, {val1:N}, {val1:F}, {val1:G}");
-Console.WriteLine($"{val2:E}, {val2:N}, {val2:F}, {val2:G}");
+//Console.WriteLine($"{val1:D}, {val1:N}, {val1:F}, {val1:G}");
+//Console.WriteLine($"{val2:E}, {val2:N}, {val2:F}, {val2:G}");
 
+//Add number after to specify precision
 //D6 = decimal with 6 digits of precision
 Console.WriteLine($"{val1:D6}, {val1:N2}, {val1:F1}, {val1:G3}");
-Console.WriteLine($"{val2:E}, {val2:N}, {val2:F}, {val2:G}");
+
+Console.WriteLine($"Sales by quarter:");
+Console.WriteLine($"{quarters[0]} {quarters[1]} {quarters[2]} {quarters[3]} ");
+Console.WriteLine($"{sales[0]} {sales[1]} {sales[2]} {sales[3]} ");
+Console.WriteLine("International sales:");
+Console.WriteLine($"{intlMixPct[0]} {intlMixPct[1]} {intlMixPct[2]} {intlMixPct[3]} ");
+
+
+
