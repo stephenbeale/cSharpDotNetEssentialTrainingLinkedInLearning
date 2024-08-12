58edda6fe1cbef62c79de74da895ee04860e713f
Steve Beale <beale.stephen@gmail.com>
2024-08-12 14:27:35 +0100

    starts with ends with

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
-﻿string str0 = String.Empty;
-Console.WriteLine(str0);
-Console.WriteLine(String.IsNullOrEmpty(str0));
-string str1 = str0 + "hello there";
-Console.WriteLine(str1);
-string[] strs = {"my", "word", "what", "an", "array" };
-string str2 = String.Join("++", strs);
-Console.WriteLine(str2);
+﻿//string str0 = String.Empty;
+//Console.WriteLine(str0);
+//Console.WriteLine(String.IsNullOrEmpty(str0));
+//string str1 = str0 + "hello there";
+//Console.WriteLine(str1);
+//string[] strs = {"my", "word", "what", "an", "array" };
+//string str2 = String.Join("++", strs);
+//Console.WriteLine(str2);
+
+//Example file for searching string content
+
+string teststr = "The quick brown fox jumps over the lazy Dog";
+
+//Contains
+
+Console.WriteLine($"{teststr.Contains("fox")}");
+Console.WriteLine($"{teststr.Contains("fox", StringComparison.CurrentCultureIgnoreCase)}");
+
+//StartsWith and EndsWith
+Console.WriteLine($"{teststr.StartsWith("the")}");
+Console.WriteLine($"{teststr.EndsWith("dog")}");
+
+//IndexOf, LastIndexOf
