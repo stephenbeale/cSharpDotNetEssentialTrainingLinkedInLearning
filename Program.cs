117022a26bda33a6244966945ae3ebc0977604f2
Steve Beale <beale.stephen@gmail.com>
2024-08-12 14:24:07 +0100

    My freestyling with string manipulation

--- Program.cs
+++ Program.cs
@@ -3,5 +3,6 @@ Console.WriteLine(str0);
 Console.WriteLine(String.IsNullOrEmpty(str0));
 string str1 = str0 + "hello there";
 Console.WriteLine(str1);
-string str2 = String.Join("++", str1);
+string[] strs = {"my", "word", "what", "an", "array" };
+string str2 = String.Join("++", strs);
 Console.WriteLine(str2);
