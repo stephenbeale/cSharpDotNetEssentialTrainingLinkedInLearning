715ce287449169abfc23d017d7b9b42148321d19
Steve Beale <beale.stephen@gmail.com>
2024-08-12 14:22:38 +0100

    Quick test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
-﻿string str0 =
+﻿string str0 = String.Empty;
 Console.WriteLine(str0);
-
+Console.WriteLine(String.IsNullOrEmpty(str0));
+string str1 = str0 + "hello there";
+Console.WriteLine(str1);
+string str2 = String.Join("++", str1);
+Console.WriteLine(str2);
