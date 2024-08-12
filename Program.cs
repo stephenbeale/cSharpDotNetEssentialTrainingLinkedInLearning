a94a3fb1e4a987e11f79c4b7926321217f349aec
Steve Beale <beale.stephen@gmail.com>
2024-08-12 11:32:57 +0100

    Moving on

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-//Declar
+//Declare some data types with values
 
-Console.WriteLine();
+int a = 1;
+char c = 'A';
+float f = 123.45f;
+decimal d = 400.85m;
+int b = default;
+bool tf = default;
+
+Console.WriteLine($"{a}, {b},{tf},{c},{f},{d}");
+Console.WriteLine($"{c + a}");
+Console.WriteLine($"{(char)(c + a)}");
+Console.WriteLine($"{f + a}");
+Console.WriteLine($"{f + c}");
+
+
+
