cdc593747c9f2af1ae6882f5b160333eb5f40a87
Steve Beale <beale.stephen@gmail.com>
2024-08-12 14:47:20 +0100

    Index of working, plus some of my freestyling

--- Program.cs
+++ Program.cs
@@ -11,16 +11,31 @@
 
 string teststr = "The quick brown fox jumps over the lazy Dog";
 
-//Contains
+////Contains
+//Console.WriteLine($"{teststr.Contains("fox")}");
+//Console.WriteLine($"{teststr.Contains("fox", StringComparison.CurrentCultureIgnoreCase)}");
 
-Console.WriteLine($"{teststr.Contains("fox")}");
-Console.WriteLine($"{teststr.Contains("fox", StringComparison.CurrentCultureIgnoreCase)}");
+////StartsWith and EndsWith
+//Console.WriteLine($"{teststr.StartsWith("the")}");
+//Console.WriteLine($"{teststr.StartsWith("the", StringComparison.CurrentCultureIgnoreCase)}");
 
-//StartsWith and EndsWith
-Console.WriteLine($"{teststr.StartsWith("the")}");
-Console.WriteLine($"{teststr.StartsWith("the", StringComparison.CurrentCultureIgnoreCase)}");
-
-Console.WriteLine($"{teststr.EndsWith("dog")}");
-Console.WriteLine($"{teststr.EndsWith("dog", StringComparison.CurrentCultureIgnoreCase)}");
+//Console.WriteLine($"{teststr.EndsWith("dog")}");
+//Console.WriteLine($"{teststr.EndsWith("dog", StringComparison.CurrentCultureIgnoreCase)}");
 
 //IndexOf, LastIndexOf
+Console.WriteLine($"{teststr.IndexOf("the")}");
+//Ignoring case finds capitalised version at start of string, hence position 0 here.
+Console.WriteLine($"{teststr.IndexOf("the", 
+StringComparison.CurrentCultureIgnoreCase)}");
+Console.WriteLine($"{teststr.LastIndexOf("the")}");
+
+//Empty, null or whitespace string
+string str1 = null;
+string str2 = "   ";
+string str3 = String.Empty;
+Console.WriteLine($"{String.IsNullOrEmpty(str1)}");
+Console.WriteLine($"{String.IsNullOrEmpty(str2)}");
+Console.WriteLine($"{String.IsNullOrEmpty(str3)}");
+Console.WriteLine($"{String.IsNullOrWhiteSpace(str2)}");
+
+Console.WriteLine($"{String.Compare(str3, str2)}");
