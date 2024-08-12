58029fe42d9549cd33796ce46d64c3350e0059bc
Steve Beale <beale.stephen@gmail.com>
2024-08-12 15:16:17 +0100

    Try parse for each type

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@ float testFloat;
 bool result;
 
 //Parse method - attempts to parse a string to a number
-foreach (string str in NumStrs)
-{
-	try
-	{
-		testFloat = float.Parse(str);
-		Console.WriteLine($"Parsed number is {testFloat}");
-        testInt = int.Parse(str);
-        Console.WriteLine($"Parsed int is {testInt}");
-    }
-	catch (FormatException e)
-	{
-		Console.WriteLine($"Could not parse '{str}': {e.Message}");
-	}
-}
+//foreach (string str in NumStrs)
+//{
+//	try
+//	{
+//		testFloat = float.Parse(str);
+//		Console.WriteLine($"Parsed number is {testFloat}");
+//        testInt = int.Parse(str);
+//        Console.WriteLine($"Parsed int is {testInt}");
+//    }
+//	catch (FormatException e)
+//	{
+//		Console.WriteLine($"Could not parse '{str}': {e.Message}");
+//	}
+//}
 
 //TryParse method returns 'true' if the parse is successful
 result = int.TryParse(NumStrs[0], out testInt);
@@ -31,3 +31,6 @@ Console.WriteLine($"{result} -- '{NumStrs[1]} : {testFloat}'");
 
 result = int.TryParse(NumStrs[2], out testInt);
 Console.WriteLine($"{result} -- '{NumStrs[2]} : {testInt}'");
+
+result = float.TryParse(NumStrs[3], out testFloat);
+Console.WriteLine($"{result} -- '{NumStrs[3]} : {testFloat}'");
