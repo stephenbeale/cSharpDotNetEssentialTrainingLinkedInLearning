2665b479d9933c5e76c297418fdebacfd57673a7
Steve Beale <beale.stephen@gmail.com>
2024-08-12 17:11:42 +0100

    DT now

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//DateTime.Now to get current Dt
+DateTime now = DateTime.Now;
+Console.WriteLine(now);
+
+//DateTime.Today
+
+//DateOnly and TimeOnly
+
+//Inspecting properties on dates
+
+//Methods on dates to change values
+
+//TimeSpan class for a duration of time
