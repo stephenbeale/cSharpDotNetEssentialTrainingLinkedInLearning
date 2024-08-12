4f6bde31008af79b312beed9c09b9e87463a9f9a
Steve Beale <beale.stephen@gmail.com>
2024-08-12 13:13:07 +0100

    Loops etc

--- Program.cs
+++ Program.cs
@@ -3,3 +3,23 @@ string str2 = "This is a string";
 string str3 = "THIS is a STRING";
 string[] strs = { "one", "two", "three", "four" };
 
+//Length of a string
+Console.WriteLine(str1.Length);
+
+//Access individual characters
+Console.WriteLine(str1[14]);
+
+//Iterate over a string
+foreach (char ch in str1)
+{
+    Console.WriteLine(ch);
+	if (ch == 'b')
+	{
+		Console.WriteLine();
+		break;
+	}
+}
+
+//String concatenation
+
+//Joining strings together
