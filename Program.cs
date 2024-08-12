9a13d0e882eca1576d62c5ca708c72913ed211b0
Steve Beale <beale.stephen@gmail.com>
2024-08-12 13:16:16 +0100

    Joining strings

--- Program.cs
+++ Program.cs
@@ -4,22 +4,28 @@ string str3 = "THIS is a STRING";
 string[] strs = { "one", "two", "three", "four" };
 
 //Length of a string
-Console.WriteLine(str1.Length);
+//Console.WriteLine(str1.Length);
 
-//Access individual characters
-Console.WriteLine(str1[14]);
+////Access individual characters
+//Console.WriteLine(str1[14]);
 
-//Iterate over a string
-foreach (char ch in str1)
-{
-    Console.WriteLine(ch);
-	if (ch == 'b')
-	{
-		Console.WriteLine();
-		break;
-	}
-}
+////Iterate over a string
+//foreach (char ch in str1)
+//{
+//    Console.Write(ch);
+//	if (ch == 'b')
+//	{
+//		Console.WriteLine();
+//		break;
+//	}
+//}
 
 //String concatenation
+string outstr;
+outstr = String.Concat(strs);
+
 
 //Joining strings together
+outstr = String.Join('.', strs);
+Console.WriteLine(outstr);
+outstr = String.Join("---", strs);
