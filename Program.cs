a5ff3df47a4bb891f5de19c715b0b249bdda740a
Steve Beale <s.j.beale@swansea.ac.uk>
2024-08-13 11:00:02 +0100

    Welsh culture now working

--- Program.cs
+++ Program.cs
@@ -28,3 +28,6 @@ Console.WriteLine("Welsh 2");
 Console.WriteLine(AprFools.ToString("D", CultureInfo.GetCultureInfoByIetfLanguageTag("en-CY")));
 Console.WriteLine("Welsh 3");
 Console.WriteLine(AprFools.ToString("D", CultureInfo.GetCultureInfoByIetfLanguageTag("cy-EN")));
+
+
+Console.WriteLine(AprFools.ToString("D", CultureInfo.GetCultureInfoByIetfLanguageTag("cy-EN")));
