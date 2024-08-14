944196020e0f88940063f9988cf583b4e60b1b46
Steve Beale <beale.stephen@gmail.com>
2024-08-14 16:34:35 +0100

    Reads a file, writes a file, outputs contents to the console

--- Program.cs
+++ Program.cs
@@ -7,11 +7,17 @@ const string filename = "TestFile.txt"; ;
 //1 WriteAllText - overwrites a file with given content
 if(!File.Exists(filename))
 {
-    return;
+    File.WriteAllText(filename, "This is a text file.");    
 }
 
 //2
 
 //3
 
-//4
+//4 
+
+//5 Read all text reads all the content from a file
+
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
