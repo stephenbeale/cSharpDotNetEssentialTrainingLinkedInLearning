7a4fc61adc6207e757b4d25405cf9114ed1b6047
Steve Beale <beale.stephen@gmail.com>
2024-08-15 14:19:00 +0100

    finally figured out path issue - was passing short path as argument, needed full path

--- Program.cs
+++ Program.cs
@@ -4,11 +4,17 @@
 
 const string dir = "StevesDirectory";
 
-if(!Directory.Exists(dir))
+string basepath = "W:\\cSharpDotNetEssentialTrainingLinkedInLearning\\";
+
+string fullPath = Path.Combine(basepath, dir);
+
+Console.WriteLine(fullPath);
+
+if (!Directory.Exists(dir))
 {
-    Console.WriteLine($"Create directory with name: {dir}");
-    //Directory.CreateDirectory($"{Directory.GetCurrentDirectory}//{dir}");
-    Directory.CreateDirectory(dir);
+    Console.WriteLine(basepath);
+    Console.WriteLine($"Create directory with name: {dir} on path {fullPath}");    
+    Directory.CreateDirectory(fullPath);
 }
 
 //Get path for current directory
