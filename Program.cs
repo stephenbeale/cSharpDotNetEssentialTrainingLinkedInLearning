e0883a85fcdc2e72ed65a77ce0a044cfda74c7fa
Steve Beale <beale.stephen@gmail.com>
2024-08-15 19:17:36 +0100

    Getting things set up

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿//Look in a given directory
 
+using System.Linq.Expressions;
+
 const string chaFilDir = "challengeFilesDir";
 const string dirSeparator = "\\";
 const string rootDirPath = "W:\\cSharpDotNetEssentialTrainingLinkedInLearning";
+const string wordExt = ".doc";
+const string excelExt = ".xslx";
+const string pptExt = ".ppt";
+
 string basePath = Directory.GetCurrentDirectory();
 
 string fullPath = Path.Combine(rootDirPath, dirSeparator, chaFilDir);
@@ -12,6 +18,41 @@ Directory.CreateDirectory(fullPath);
 //Analyse file contents
 
 //Print out
+try
+{
+    if (Directory.Exists(fullPath))
+    {
+        string[] files = Directory.GetFiles(fullPath);
+        Console.WriteLine("Results");
+        Console.WriteLine($"Total Files: {files.Length}");
+
+        int wordFileCount = 0;
+        int excelFileCount = 0;
+        int powerpointFileCount = 0;
+
+        List<string> extensions = new List<string>();
+
+        extensions.Add(wordExt);
+        extensions.Add(excelExt);
+        extensions.Add(pptExt);            
+
+        int wordFileSize = 0;
+        int excelFileSize = 0;
+        int powerpointFileSize = 0;
+
+        foreach (string file in files)
+        {
+            if (file.Contains(wordExt))
+            {
+                
+            }
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Exception caught: {e.Message}");
+}
 
 /*
  * Results
