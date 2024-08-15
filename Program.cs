6ec90b6135256036cc0fe5ee0127df1cec01fa10
Steve Beale <beale.stephen@gmail.com>
2024-08-15 15:11:59 +0100

    Trying more on getting info about a directory

--- Program.cs
+++ Program.cs
@@ -13,13 +13,38 @@ Console.WriteLine(fullPath);
 if (!Directory.Exists(dir))
 {
     Console.WriteLine(basepath);
-    Console.WriteLine($"Create directory with name: {dir} on path {fullPath}");    
-    Directory.CreateDirectory(fullPath);
+    Console.WriteLine($"Create directory with name: {dir} on path {fullPath}");
+    Directory.CreateDirectory(dir);
+    //Directory.CreateDirectory(fullPath);
+}
+else
+{
+    Directory.Delete(dir);
 }
 
 //Get path for current directory
+Console.WriteLine(Path.GetDirectoryName(dir));
 
 //Get info about a directory
+try
+{
+    Console.WriteLine($"Directory files: {Directory.GetFileSystemEntries(basepath)}");    
+    Console.WriteLine($"Directory parent of {dir}: {Directory.GetParent(dir)}");
+    Console.WriteLine($"Directory creation time of {dir} : {Directory.GetCreationTime(dir)}");
+    Console.WriteLine($"Directory root of {dir} : {Directory.GetDirectoryRoot(dir)}");
+    Console.WriteLine($"\nDirectory files in base path of {basepath}: \n");
+    string[] files = Directory.GetFiles(basepath);    
+    foreach (var file in files)
+    {
+        Console.WriteLine($"{file}");        
+        Console.WriteLine($"Path file name: {Path.GetFileName(file)}");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
 
 //Enumerate contents of directories
 
