16443a5b95026f5c8c94753ba883a13e1f494753
Steve Beale <beale.stephen@gmail.com>
2024-08-15 11:14:56 +0100

    My guesses not working so well

--- Program.cs
+++ Program.cs
@@ -1,66 +1,22 @@
-﻿//Working with file information
+﻿//Working with directories
 
-//Make sure the example file exists
+//Create directory if it doesn't exist
 
-const string filename = "TestFile.txt";
+const string dir = "StevesDirectory";
 
-if (!File.Exists(filename))
+if(!Directory.Exists(dir))
 {
-    using (StreamWriter sw = File.CreateText(filename))
-    {
-        sw.WriteLine("This is a text file.");
-    }
+    Console.WriteLine($"Create directory with name: {dir}");
+    //Directory.CreateDirectory($"{Directory.GetCurrentDirectory}//{dir}");
+    Directory.CreateDirectory(dir);
 }
 
-//Get details about the files
+//Get path for current directory
 
-//Console.WriteLine(File.GetCreationTime(filename));
-//Console.WriteLine(File.GetLastWriteTime(filename)); Console.WriteLine(File.GetLastAccessTime(filename));
+//Get info about a directory
 
-//Make file read-only and print attributes
-//File.SetAttributes(filename, FileAttributes.ReadOnly);
-//Console.WriteLine(File.GetAttributes(filename));
+//Enumerate contents of directories
 
-////My playing
-//File.SetAttributes(filename, FileAttributes.IntegrityStream);
-
-//Console.WriteLine(File.GetAttributes(filename));
-
-
-//Get general file info
-
-//try
-//{
-//	FileInfo fi = new FileInfo(filename);
-//	Console.WriteLine($"File length: {fi.Length}");
-//    Console.WriteLine($"File directory: {fi.Directory}"); 
-//	Console.WriteLine($"File is read-only: {fi.IsReadOnly}");
-//}
-//catch (Exception e)
-//{
-//	Console.WriteLine(e.Message);
-//}
-
-//Manipulate file information
-
-DateTime dt = new DateTime(2020, 7, 1);
-File.SetCreationTime(filename, dt);
-
-Console.WriteLine($"File's new creation time: {File.GetCreationTime(filename)}");
-
-//Mine
-File.SetLastAccessTime(filename, (new DateTime(2020, 1, 12, 19, 45, 03)));
-Console.WriteLine($"File's new last access time: {File.GetLastAccessTime(filename)}");
-
-//Should copy?
-const string filename2 = "myCopiedFile.txt";
-File.Copy(filename, filename2);
-
-Console.WriteLine($"Creation time of {filename2}: {File.GetCreationTime(filename2)}");
-
-Console.WriteLine("Moving a file");
-File.Move(filename, "movedFile.txt");
-
-File.SetCreationTime(filename2, (new DateTime(1787, 9, 23, 15, 04, 42, 38)));
-Console.WriteLine($"Modified creation time for {filename2}: {File.GetCreationTime(filename2)}");
+//
+//Creating and deleting directories
 
