d5e3fa63bdcf907a0ef0655bcb02dd16b17a4050
Steve Beale <beale.stephen@gmail.com>
2024-08-15 10:29:59 +0100

    Getting file info

--- Program.cs
+++ Program.cs
@@ -4,24 +4,40 @@
 
 const string filename = "TestFile.txt";
 
-if (!File.Exists(filename))
+//if (!File.Exists(filename))
+//{
+//    using (StreamWriter sw = File.CreateText(filename))
+//    {
+//        sw.WriteLine("This is a text file.");
+//    }
+//}
+
+////Get details about the files
+
+//Console.WriteLine(File.GetCreationTime(filename));
+//Console.WriteLine(File.GetLastWriteTime(filename)); Console.WriteLine(File.GetLastAccessTime(filename));
+
+////Make file read-only and print attributes
+//File.SetAttributes(filename, FileAttributes.ReadOnly);
+//Console.WriteLine(File.GetAttributes(filename));
+
+////My playing
+//File.SetAttributes(filename, FileAttributes.IntegrityStream);
+
+//Console.WriteLine(File.GetAttributes(filename));
+
+
+//Get general file info
+
+try
 {
-    using (StreamWriter sw = File.CreateText(filename))
-    {
-        sw.WriteLine("This is a text file.");
-    }
+	FileInfo fi = new FileInfo(filename);
+	Console.WriteLine($"{fi.Length}");
+    Console.WriteLine($"{fi.Directory}"); 
+	Console.WriteLine($"{fi.IsReadOnly}");
 }
+catch (Exception e)
+{
 
-//Get details about the files
-
-Console.WriteLine(File.GetCreationTime(filename));
-Console.WriteLine(File.GetLastWriteTime(filename)); Console.WriteLine(File.GetLastAccessTime(filename));
-
-//Make file read-only and print attributes
-File.SetAttributes(filename, FileAttributes.ReadOnly);
-Console.WriteLine(File.GetAttributes(filename));
-
-//My playing
-File.SetAttributes(filename, FileAttributes.IntegrityStream);
-
-Console.WriteLine(File.GetAttributes(filename));
+	Console.WriteLine(e.Message);
+}
