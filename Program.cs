f324d61eb00509a9a3d1e01dc9f9c0a378c7fd0a
Steve Beale <beale.stephen@gmail.com>
2024-08-15 18:19:23 +0100

    Notes on challenge requirements

--- Program.cs
+++ Program.cs
@@ -1,66 +1,24 @@
-﻿//Working with file information
+﻿//Look in a given directory
 
-//Make sure the example file exists
+//Analyse file contents
 
-const string filename = "TestFile.txt";
+//Print out
 
-if (!File.Exists(filename))
-{
-    using (StreamWriter sw = File.CreateText(filename))
-    {
-        sw.WriteLine("This is a text file.");
-    }
-}
+/*
+ * Results
+ * with bulleted list of 
+ * Total files
+ * Excel files: #
+ * Word files: #
+ * PPT files: #
+ * ----
+ */
 
-//Get details about the files
+/*
+ * Total size of all files
+ * Excel size:
+ * Word size:
+ * PPT size: 
+ */
 
-//Console.WriteLine(File.GetCreationTime(filename));
-//Console.WriteLine(File.GetLastWriteTime(filename)); Console.WriteLine(File.GetLastAccessTime(filename));
-
-//Make file read-only and print attributes
-//File.SetAttributes(filename, FileAttributes.ReadOnly);
-//Console.WriteLine(File.GetAttributes(filename));
-
-////My playing
-//File.SetAttributes(filename, FileAttributes.IntegrityStream);
-
-//Console.WriteLine(File.GetAttributes(filename));
-
-
-//Get general file info
-
-//try
-//{
-//	FileInfo fi = new FileInfo(filename);
-//	Console.WriteLine($"File length: {fi.Length}");
-//    Console.WriteLine($"File directory: {fi.Directory}"); 
-//	Console.WriteLine($"File is read-only: {fi.IsReadOnly}");
-//}
-//catch (Exception e)
-//{
-//	Console.WriteLine(e.Message);
-//}
-
-//Manipulate file information
-
-DateTime dt = new DateTime(2020, 7, 1);
-File.SetCreationTime(filename, dt);
-
-Console.WriteLine($"File's new creation time: {File.GetCreationTime(filename)}");
-
-//Mine
-File.SetLastAccessTime(filename, (new DateTime(2020, 1, 12, 19, 45, 03)));
-Console.WriteLine($"File's new last access time: {File.GetLastAccessTime(filename)}");
-
-//Should copy?
-const string filename2 = "myCopiedFile.txt";
-File.Copy(filename, filename2);
-
-Console.WriteLine($"Creation time of {filename2}: {File.GetCreationTime(filename2)}");
-
-Console.WriteLine("Moving a file");
-File.Move(filename, "movedFile.txt");
-
-File.SetCreationTime(filename2, (new DateTime(1787, 9, 23, 15, 04, 42, 38)));
-Console.WriteLine($"Modified creation time for {filename2}: {File.GetCreationTime(filename2)}");
-
+//Should also run for any newly added files
