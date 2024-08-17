3c54a4d71b6d5f047cd84e27de1e22080a3aead5
Steve Beale <beale.stephen@gmail.com>
2024-08-17 10:10:14 +0100

    Matching functions in Regex testing

--- Program.cs
+++ Program.cs
@@ -1,74 +1,42 @@
-﻿//Look in a given directory
+﻿//Regex here we come
 
-var folder = "StevesDirectory";
-var resultsFile = "results.txt";
+//Must add this using statement explicitly
+using System.Text.RegularExpressions;
 
-//Variables to ohld results
-long XLSCount = 0, DOCCount = 0, PPTCount = 0;
-long XLSSize = 0, DOCSize = 0, PPTSize = 0;
-long totalFiles = 0, totalSize = 0;
+string teststr1 = "The quick brown Fox jumps over the lazy Dog";
+string teststr2 = "the quick brown fox jumps over the lazy dog";
 
+//IsMatch function for content of a string
 
-bool IsOfficeFile(string fileName)
+//Checks if a string has capitals, returns boolean
+Regex CapWords = new Regex(@"[A-Z]\w+");
+
+Console.WriteLine(CapWords.IsMatch(teststr1));
+Console.WriteLine(CapWords.IsMatch(teststr2));
+
+//Ignore case in searchs
+
+Regex NoCase = new Regex(@"fox", RegexOptions.IgnoreCase);
+
+Console.WriteLine(NoCase.IsMatch(teststr1));
+Console.WriteLine(NoCase.IsMatch(teststr2));
+
+//Match - returns one result
+
+Match m = CapWords.Match(teststr1);
+Console.WriteLine();
+while(m.Success)
 {
-    if(fileName.EndsWith(".xlsx") || fileName.EndsWith(".docx") || fileName.EndsWith(".pptx"))
-    {
-        return true;
-    }
-    return false;
+    Console.WriteLine($"'{m.Value}' found at  position {m.Index}");
+    m = m.NextMatch();
 }
 
-try
+//Matches = multiple matches
+
+MatchCollection mc = CapWords.Matches(teststr1);
+Console.WriteLine($"Found {mc.Count} matches:");
+
+foreach ( Match match in mc)    
 {
-    //DI for given folder
-    DirectoryInfo di = new DirectoryInfo(folder);
-
-    foreach (FileInfo fi in di.EnumerateFiles())
-    {
-        if (IsOfficeFile(fi.Name))
-        {
-            totalFiles++;
-            totalSize += fi.Length;
-            if (fi.Name.EndsWith(".xlsx"))
-            {
-                XLSCount++;
-                XLSSize += fi.Length;
-            }
-            if (fi.Name.EndsWith("docx"))
-            {
-                DOCCount++;
-                DOCSize += fi.Length;
-            }
-            if (fi.Name.EndsWith("pptx"))
-            {
-                PPTCount++;
-                PPTSize += fi.Length;
-            }
-        }
-    }
-
-    //Output results
-
-    using (StreamWriter sw = File.CreateText(resultsFile))
-    {
-        sw.WriteLine("===== Results =======");
-        sw.WriteLine($"Total files: {totalFiles}");
-        sw.WriteLine($"Excel Count: {XLSCount}"); 
-        sw.WriteLine($"Word Count: {DOCCount}"); 
-        sw.WriteLine($"PPT Count: {PPTCount}\n"); 
-        sw.WriteLine($"Total size: {totalSize:N0}"); 
-        sw.WriteLine($"Excel size: {XLSSize:N0}"); 
-        sw.WriteLine($"Word size: {DOCSize:N0}");
-        sw.WriteLine($"PPT size: {PPTSize:N0}");
-    }
-
-}
-//Analyse file contents
-
-//Print out
-
-
-catch (Exception e)
-{
-    Console.WriteLine($"Exception caught: {e.Message}");
+    Console.WriteLine($"'{match.Value}' found at position {match.Index}");
 }
