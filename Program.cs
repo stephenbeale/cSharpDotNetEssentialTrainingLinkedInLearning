735b281edc9fadaaa22fd7049f8fb7f2817e5d39
Steve Beale <beale.stephen@gmail.com>
2024-08-19 13:03:46 +0100

    Make upper using match function

--- Program.cs
+++ Program.cs
@@ -10,3 +10,13 @@ Regex CapWords = new Regex(@"[A-Z]\w+");
 string result = CapWords.Replace(teststr1, "***");
 Console.WriteLine(teststr1);
 Console.WriteLine(result);
+
+//Function to generate replacement text on the fly according to logic - using a delegate
+//function takes match object as an argument, returns a string as a result
+string MakeUpper(Match m)
+{
+    string s = m.ToString();
+    return s.ToUpper();
+}
+
+
