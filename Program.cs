c7bd04839e6d5e30c420c2935434d8f9ffb2b02e
Steve Beale <beale.stephen@gmail.com>
2024-08-19 13:00:03 +0100

    Replacements working

--- Program.cs
+++ Program.cs
@@ -7,3 +7,6 @@ string teststr1 = "The quick brown Fox jumps over the lazy Dog";
 Regex CapWords = new Regex(@"[A-Z]\w+");
 
 //Replace string contents
+string result = CapWords.Replace(teststr1, "***");
+Console.WriteLine(teststr1);
+Console.WriteLine(result);
