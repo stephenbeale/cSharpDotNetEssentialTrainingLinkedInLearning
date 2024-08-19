8a41e432d41aba8d8301e1791fd44272d0931161
Steve Beale <beale.stephen@gmail.com>
2024-08-19 13:17:52 +0100

    Match function to replace now working

--- Program.cs
+++ Program.cs
@@ -16,7 +16,18 @@ Console.WriteLine(result);
 string MakeUpper(Match m)
 {
     string s = m.ToString();
+    
+    //If match is at start of sentence, do nothing.
+    if(m.Index == 0 )
+    {
+        return s;
+    }
     return s.ToUpper();
 }
+
+var upperStr = CapWords.Replace(teststr1, new MatchEvaluator(MakeUpper));
+Console.WriteLine("\nCapWords Replace using MakeUpper");
+Console.WriteLine($"Original: {teststr1}");
+Console.WriteLine($"Capitalised: {upperStr}");
 
 
