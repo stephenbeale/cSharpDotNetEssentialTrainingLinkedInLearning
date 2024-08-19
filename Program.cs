7d8ff1e8af039cd5292f7d00756520cbf6b56963
Steve Beale <beale.stephen@gmail.com>
2024-08-19 13:33:33 +0100

    Stopwatch timeout throws exception as I've set it to 1 millisecond

--- Program.cs
+++ Program.cs
@@ -7,13 +7,16 @@ const string thestr = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 Stopwatch sw;
 
 //Use a timeout value when executing RegEx to guard against bad input
+const int MAX_REGEX_TIME = 1;
+//1 second timeout added
+TimeSpan timeout = TimeSpan.FromMilliseconds(MAX_REGEX_TIME);
 
 //Run the expression and output the result
-
 try
 {
 	sw = Stopwatch.StartNew();
-	Regex CapWords = new Regex(@"(a+a+)+b", RegexOptions.None);
+	//If execution time of regex exceeds max. time I've set, must throw an exception
+	Regex CapWords = new Regex(@"(a+a+)+b", RegexOptions.None, timeout);
 	MatchCollection mc = CapWords.Matches(thestr);
 	sw.Stop();
 	Console.WriteLine($"Found {mc.Count} matches in {sw.Elapsed} time.");
@@ -23,7 +26,7 @@ try
 		Console.WriteLine($"'{match.Value}' found at position {match.Index}");
 	}
 }
-catch (Exception e)
+catch (RegexMatchTimeoutException e)
 {
-	Console.WriteLine(e.Message);
+	Console.WriteLine($"The Regex took too long to execute! {e.Message}");
 }
